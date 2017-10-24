919cce46f1ffcb56591f61ff2f6098e58182a241
Arylo <arylo.open@gmail.com>
2017-10-25 01:43:22 +0800

    Update `.d.ts` File And Add Custom Config Module Example

---
+++ examples/custom_config_module/index.ts
@@ -0,0 +1,4 @@
+import config = require('config');
+
+const test = config.test;
+const bar = config.bar;
--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,7 @@
-interface AnyObject {
-  [prop: string]: any
-}
+import { Config, AnyObject } from './dts.d';
 
 declare module "y-config" {
-  function getDebug(): boolean;
-  function setDebug(bool: boolean): boolean;
-
-  function on(event: string, cb: any): void;
-  function once(event: string, cb: any): void;
-
-  function getConfig(): AnyObject;
-  function setConfig(obj: any): boolean;
-
-  function clearConfigCwd(): boolean;
-  function getConfigCwd(): boolean;
-  function setConfigCwd(cwd: string): boolean;
-
-  function clearConfigPath(): boolean;
-  function clearCustomConfigPath(): boolean;
-  function getConfigPath(): string;
-  function getCustomConfigPath(): string;
-  function setConfigPath(filepath: string): boolean;
-  function setCustomConfigPath(filepath: string): boolean;
-
-  function clear(): boolean;
-  function reload(): boolean;
+  export = new Config();
 }
 
-declare let config: "y-config";
-export = config;
+export = new Config();

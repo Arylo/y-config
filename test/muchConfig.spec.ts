7e57814fb8436896a39a79f6007578d718208535
Arylo <arylo.open@gmail.com>
2018-06-22 17:19:23 +0800

    Add the feature of Custom Parser and add any test unit

--- test/muchConfig.spec.ts
+++ test/muchConfig.spec.ts
@@ -1,8 +1,12 @@
 import test from "ava";
+import * as findUp from "find-up";
+import * as path from "path";
 import Config = require("../lib");
 
-test("Empty Config", (t) => {
-    const oneConfig = new Config();
+const configFolderPath = path.resolve(findUp.sync("simples"), "config");
+
+test("Much Config", (t) => {
+    const oneConfig = new Config<{ baz: string; }>();
     const otherConfig = new Config();
     oneConfig.addConfig({
         baz: "foo"
@@ -10,4 +14,14 @@ test("Empty Config", (t) => {
     t.not(oneConfig.toString(), otherConfig.toString());
     t.not(oneConfig.toObject(), otherConfig.toObject());
     t.not(oneConfig.getConfig(), otherConfig.getConfig());
+    t.is(oneConfig.get("baz"), "foo");
+    t.is(otherConfig.get("baz" as never), undefined);
+});
+
+test("Much Config File", (t) => {
+    const config = new Config<{ type: string; comment: string; }>();
+    config.addConfigPath(path.resolve(configFolderPath, "config.json"));
+    t.is(config.get("type"), "JSON");
+    config.addConfigPath(path.resolve(configFolderPath, "config.yaml"));
+    t.is(config.get("type"), "YAML");
 });
